080277455f2da5e22378158be4261e0547b369b3
tu-Rahat <toslimuddinrahat@gmail.com>
2026-05-20 21:21:28 +0600

    recursion 2 type added

--- recursion/print_m_to_n.py
+++ recursion/print_m_to_n.py
@@ -9,7 +9,7 @@ def printing(m,n):
     print(f"{m}-->",end="")
     printing(m+1,n) 
 
-
+# recursion tail case
 def backprint(m,n):
     if m == n:
         print(m,end="")    # 1 2 3 4 5 6 7 8 9
@@ -17,13 +17,29 @@ def backprint(m,n):
         backprint(m+1,n)
         print(f"<--{m}",end="")
 
+# recursion head case  
+def head_backprint(m,n):
+    if n == m:
+        print(m,end="")
+        return
+    else:
+        print(f"{n}--",end="") 
+        head_backprint(m,n-1) 
 
-m,n = [int(x) for x in input().split(" ")] 
-printing(m,n)
-
-print("-------<<---------->>-------------")
-print("Reverse print") 
-
-backprint(m,n) 
-
+m,n = [int(x) for x in input("2 number and space,ex: 1 10 :>").split(" ")] 
+while True:
+    option = int(input("choose between 1 to 3: "))
+    print()
+    if option == 1:
+        printing(m,n)
+  
+    elif option == 2:
+        backprint(m,n) 
+        print()
+    elif option == 3:
+        head_backprint(m,n) 
+        print()
+    else:
+        print("Exit....")
+        break
 
